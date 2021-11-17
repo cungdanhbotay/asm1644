7cfd6227068ea3e5ad8b528e98257dbe66a67916
baophan1223 <cungdanhbotay@gmail.com>
2021-11-17 18:23:28 +0700

    .

--- shopping_iPhone_online-20211105T201753Z-001/shopping_iPhone_online/connection.php
+++ shopping_iPhone_online-20211105T201753Z-001/shopping_iPhone_online/connection.php
@@ -1,3 +1,7 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'shoping_online') or die("Can not connect database".mysqli_connect_error());
+    $conn = pg_connect("postgres://rntxlyyxnzhpaf:e980fafd84975b3668d45257a79e58c5de9bb4daba12db767c4901e0eea64910@ec2-54-146-82-179.compute-1.amazonaws.com:5432/ddsahi6cstee4r");
+
+    if(!$conn){
+        die("Can not connect database");
+    } 
 ?>
