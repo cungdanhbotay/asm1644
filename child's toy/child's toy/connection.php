e0370a89117c362ea71d6244bacdc4494f930c69
baophan1223 <cungdanhbotay@gmail.com>
2021-11-22 10:12:28 +0700

    .

--- child's toy/child's toy/connection.php
+++ child's toy/child's toy/connection.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = pg_connect("postgres://rntxlyyxnzhpaf:e980fafd84975b3668d45257a79e58c5de9bb4daba12db767c4901e0eea64910@ec2-54-146-82-179.compute-1.amazonaws.com:5432/ddsahi6cstee4r");
+    $conn = pg_connect("postgres://uvhzsnhlscjcvu:9af6452855fa45b0392512c7f33e95fa8e8034a8f5511a4392373158326f3286@ec2-107-23-213-65.compute-1.amazonaws.com:5432/d64h8hcu1o61fl");
 
     if(!$conn){
         die("Can not connect database");
